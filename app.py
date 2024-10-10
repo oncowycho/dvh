892682bb5800cfba102adecd044a81902df8cc70
oncowycho <143578451+oncowycho@users.noreply.github.com>
2024-10-10 15:11:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -119,8 +119,8 @@ def main():
     
     if rtss_file is not None and rtdose_file is not None:
         # Read the DICOM files using pydicom
-        # rtss = pydicom.dcmread(rtss_file)
-        # rtdose = pydicom.dcmread(rtdose_file)
+        rtss_file = pydicom.dcmread(rtss_file)
+        rtdose_file = pydicom.dcmread(rtdose_file)
 
         rtss_parser = dicomparser.DicomParser(rtss_file)
        
